2e4baa3bdc562c73ea6b2b389dc95266fb61d181
Hibiki Chiba <Hibiki@hoge.com>
2023-10-19 12:58:16 +0900

    Addendum about copyright

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # robosys2023
 Program created in class
+
+* このソフトウェアパッケージは、3条項BSDライセンスの下、再配布および使用が許可されています. 
+* © 2023 Hibiki Chiba
--- plus_stdin.py
+++ plus_stdin.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# SPDX-FileCopyrightaText: 2023 Hibiki Chiba
+# SPDX-License-Identifier: BSD-3-Clause
 
 import sys
 
