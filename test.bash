e44ddf9778b31c3e5e3d28482ada96884da9371f
Hibiki Chiba <Hibiki@hoge.com>
2023-11-02 14:48:48 +0900

    Regression test

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Hibiki Chiba
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line
+	res=1
+}
+
+ret=0
+
+### I/O TEST ###
+out=$(seq 5 | python3 plus.py)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
