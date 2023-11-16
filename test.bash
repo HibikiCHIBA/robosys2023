2ca7d565f12eb94efdf5c30a3a01859d02aecfde
Hibiki Chiba <Hibiki@hoge.com>
2023-11-16 14:45:19 +0900

    fix

--- test.bash
+++ test.bash
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line $1
+ 	echo NG at Line $1
 	res=1
 }
 
 ret=0
 
 ### I/O TEST ###
-out=$(seq 5 | python3 plus.py)
+out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
