e716b4007ec587dd2d15b794db87d352a683ace2
Hibiki Chiba <Hibiki@hoge.com>
2023-11-16 13:10:25 +0900

    fix

--- test.bash
+++ test.bash
@@ -2,6 +2,16 @@
 # SPDX-FileCopyrightText: 2023 Hibiki Chiba
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+ret=0
+
 ### I/O TEST ###
 out=$(seq 5 | python3 plus.py)
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
